8dce021e58345e513d29ff0a9c9b6214afc75cc7
bmangust <ftf.pollux@gmail.com>
2020-12-24 14:03:32 +0300

    add Search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,13 @@
-import { FormControl, Grid, InputBase, makeStyles } from "@material-ui/core";
+import {
+  Button,
+  Fade,
+  FormControl,
+  Grid,
+  InputBase,
+  makeStyles,
+  Paper,
+  Popper,
+} from "@material-ui/core";
 import { SearchRounded } from "@material-ui/icons";
 import React, { useState } from "react";
 import { theme } from "../../theme";
@@ -11,6 +20,9 @@ const useStyles = makeStyles({
     border: `1px solid ${theme.palette.grey[500]}`,
     borderRadius: 5,
     maxWidth: 300,
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
   },
   Input: {
     padding: "10px 1rem",
@@ -23,12 +35,28 @@ const useStyles = makeStyles({
     fontSize: "2rem",
     color: theme.palette.grey[700],
     paddingRight: 5,
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1.6rem",
+      paddingRight: 0,
+    },
+  },
+  Button: {
+    margin: "10px",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  Popper: {
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
   },
 });
 
 const Search = (props: Props) => {
   const classes = useStyles();
   const [search, setSearch] = useState("");
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -39,6 +67,12 @@ const Search = (props: Props) => {
       setSearch("");
     }
   };
+  const handleOpenSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setAnchorEl(anchorEl ? null : e.currentTarget);
+  };
+  const open = Boolean(anchorEl);
+  const id = open ? "search-popover" : undefined;
 
   return (
     <FormControl>
@@ -59,6 +93,38 @@ const Search = (props: Props) => {
         />
         <SearchRounded className={classes.Icon} />
       </Grid>
+
+      <Button
+        aria-describedby={id}
+        className={classes.Button}
+        onClick={handleOpenSearch}
+        variant="outlined"
+      >
+        <SearchRounded className={classes.Icon} />
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorEl}
+        placement={"bottom-end"}
+        transition
+        className={classes.Popper}
+      >
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={250}>
+            <Paper>
+              <InputBase
+                className={classes.Input}
+                inputProps={{ "aria-label": "search for a movie" }}
+                value={search}
+                classes={{ input: classes.BaseInput }}
+                onChange={handleInput}
+                onKeyPress={handleSearch}
+                placeholder="Search..."
+              />
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
     </FormControl>
   );
 };
