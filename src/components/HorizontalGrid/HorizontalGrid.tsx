1e94f4feb8230a0a06536e203a40486225db540d
bmangust <ftf.pollux@gmail.com>
2021-01-17 12:34:53 +0300

    add HorizontalGrid

---
+++ src/components/HorizontalGrid/HorizontalGrid.tsx
@@ -0,0 +1,68 @@
+import { Grid, makeStyles } from '@material-ui/core';
+import React from 'react';
+import CategoryHeader from '../CategoryHeader/CategoryHeader';
+
+interface Props {
+  name: string;
+  sources?: string[];
+  type: 'photo' | 'video';
+}
+interface GridItemProps {
+  name: string;
+  src: string;
+  type: 'photo' | 'video';
+  key?: string | number;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '10px 0',
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
+  },
+  HorizontalScroll: {
+    overflowX: 'auto',
+  },
+  Image: {
+    height: '10rem',
+    marginRight: 10,
+  },
+}));
+
+const GridItem: React.FC<GridItemProps> = ({
+  src,
+  name,
+  type,
+}: GridItemProps) => {
+  const classes = useStyles();
+
+  switch (type) {
+    case 'photo':
+      return <img src={src} alt={name} className={classes.Image} />;
+    case 'video':
+      return <video controls src={src} className={classes.Image} />;
+    default:
+      return null;
+  }
+};
+
+const HorizontalGrid = ({ sources, name, type }: Props) => {
+  const classes = useStyles();
+
+  return (
+    <Grid container direction="column" className={classes.root}>
+      <CategoryHeader text={`${type}s`} />
+      <Grid container wrap="nowrap" className={classes.HorizontalScroll}>
+        {sources?.length
+          ? sources.map((item: string) => (
+              <GridItem key={item} src={item} name={name} type={type} />
+            ))
+          : `No ${type}s`}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HorizontalGrid;
