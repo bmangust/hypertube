9ed21f71f9653ecb56081e113077d3c49a18a573
bmangust <ftf.pollux@gmail.com>
2021-02-01 02:28:25 +0300

    add dynamic i18n button fontSize

--- src/components/Internationalization/Internationalization.tsx
+++ src/components/Internationalization/Internationalization.tsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import { theme } from '../../theme';
 
 const languages = ['en', 'ru'];
 
+const useStyles = makeStyles({
+  Button: {
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '0.7rem',
+    },
+  },
+});
+
 const Internationalization = () => {
+  const classes = useStyles();
   const [lang, setLang] = useState(0);
   const nextLang = (lang + 1) % languages.length;
   const { t, i18n } = useTranslation();
@@ -15,9 +25,9 @@ const Internationalization = () => {
   };
   return (
     <div>
-      <Button onClick={handleClick}>{`${t('translate')} ${t(
-        languages[nextLang]
-      )}`}</Button>
+      <Button onClick={handleClick} className={classes.Button}>{`${t(
+        'translate'
+      )} ${t(languages[nextLang])}`}</Button>
     </div>
   );
 };
